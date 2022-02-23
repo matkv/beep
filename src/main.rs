544b2c6ed454e0607f734ddf17fc1888417afda4
Matko <matkobusljeta@gmail.com>
2022-02-23 19:57:10 +0100

    Finished first working version

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
+use std::{thread, time::Duration};
+
+use rodio::{
+    source::{Amplify, SineWave, TakeDuration},
+    OutputStream, Sink, Source,
+};
 use tray_item::TrayItem;
 
 fn main() {
+    setup_tray_item();
+}
+
+fn setup_tray_item() {
     gtk::init().unwrap();
 
-    let mut tray = TrayItem::new("Tray Example", "accessories-calculator").unwrap();
+    let mut tray = TrayItem::new("Beep", "accessories-calculator").unwrap();
 
-    tray.add_label("Tray Label").unwrap();
+    tray.add_menu_item("Beep once", || {
+        play_sound();
+    })
+    .unwrap();
 
-    tray.add_menu_item("Hello", || {
-        println!("Hello!");
+    tray.add_menu_item("Beep twice", || {
+        for _number in 0..2 {
+            play_sound();
+            thread::sleep(Duration::from_millis(500));
+        }
     })
     .unwrap();
 
@@ -18,4 +34,21 @@ fn main() {
     .unwrap();
 
     gtk::main();
+}
+
+fn play_sound() {
+    let sinewave = setup_sinewave();
+
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let sink = Sink::try_new(&stream_handle).unwrap();
+    sink.append(sinewave);
+    sink.sleep_until_end();
+}
+
+fn setup_sinewave() -> Amplify<TakeDuration<SineWave>> {
+    let sine = SineWave::new(340.0)
+        .take_duration(Duration::from_secs_f32(0.75))
+        .amplify(0.75);
+
+    return sine;
 }
