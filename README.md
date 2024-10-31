6f47bbf27ace034b9c7aa980eae5c7a2252807dd
matkv <mail@matkv.dev>
2024-10-31 18:55:18 +0100

    set up sys tray icon

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 A small program that plays a sound when clicking an icon in the system tray / tray bar. It is used to wake up my studio monitor speakers from the sleep mode.
 
 Currently rewriting this in Go as a learning project.
+
+## Notes 
+
+Library used for system tray icon: https://github.com/getlantern/systray
+
+To avoid opening a console at application start, use this compile flag (for Windows):
+
+```bash
+go build -ldflags -H=windowsgui
+```
--- main.go
+++ main.go
@@ -1,5 +1,48 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/getlantern/systray"
+	"github.com/getlantern/systray/example/icon"
+)
+
 func main() {
-	println("beep")
+	systray.Run(onReady, onExit)
+}
+
+func onReady() {
+	setup()
+}
+
+func setup() {
+	systray.SetTitle("Beep")
+	systray.SetTooltip("Play a sound to wake up the studio monitors")
+	systray.SetIcon(icon.Data) // TODO - change this to a custom icon
+
+	beepButton := systray.AddMenuItem("Play Beep", "Play a beep sound")
+	quitButton := systray.AddMenuItem("Quit", "Exit the application")
+
+	go func() {
+		for {
+			<-beepButton.ClickedCh
+			playBeep() // Play sound when "Play Beep" button is clicked
+		}
+	}()
+
+	go func() {
+		for {
+			<-quitButton.ClickedCh
+			systray.Quit()
+			os.Exit(0)
+		}
+	}()
+}
+
+func playBeep() {
+	fmt.Println("Playing beep sound!")
+}
+
+func onExit() {
 }
