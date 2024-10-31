4f956597f574f2c2e59722ac6641be159352eb8f
matkv <mail@matkv.dev>
2024-10-31 20:54:43 +0100

    added custom icons + small cleanup

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	"github.com/getlantern/systray"
-	"github.com/getlantern/systray/example/icon"
 	"github.com/gopxl/beep"
 	"github.com/gopxl/beep/speaker"
 	"github.com/gopxl/beep/wav"
@@ -17,6 +16,15 @@ func main() {
 	systray.Run(onReady, onExit)
 }
 
+//go:embed beep.wav
+var soundFile embed.FS
+
+//go:embed icons/play.ico
+var playIcon []byte
+
+//go:embed icons/close.ico
+var closeIcon []byte
+
 func onReady() {
 	setup()
 }
@@ -24,10 +32,13 @@ func onReady() {
 func setup() {
 	systray.SetTitle("Beep")
 	systray.SetTooltip("Play a sound to wake up the studio monitors")
-	systray.SetIcon(icon.Data) // TODO - change this to a custom icon
+	systray.SetIcon(playIcon) // TODO - change this to a custom icon
 
 	beepButton := systray.AddMenuItem("Play Beep", "Play a beep sound")
-	quitButton := systray.AddMenuItem("Quit", "Exit the application")
+	beepButton.SetIcon(playIcon)
+
+	closeButton := systray.AddMenuItem("Quit", "Exit the application")
+	closeButton.SetIcon(closeIcon)
 
 	go func() {
 		for {
@@ -38,15 +49,13 @@ func setup() {
 
 	go func() {
 		for {
-			<-quitButton.ClickedCh
+			<-closeButton.ClickedCh
 			systray.Quit()
 			os.Exit(0)
 		}
 	}()
 }
 
-//go:embed beep.wav
-var soundFile embed.FS
 var speakerInitialized bool
 
 func playSound() {
@@ -58,14 +67,12 @@ func playSound() {
 		speakerInitialized = true
 	}
 
-	// Open the embedded sound file
 	f, err := soundFile.Open("beep.wav")
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close()
 
-	// Decode the WAV file
 	decoder, _, err := wav.Decode(f)
 	if err != nil {
 		panic(err)
@@ -80,7 +87,7 @@ func playSound() {
 		wg.Done() // playback done
 	})))
 
-	// Wait for the sound to finish
+	// wait for sound to finish
 	wg.Wait()
 }
 
